afc0e832defc0b9f773f3c2ccef2638f972c0969
Coen Schutte <coenschutte@gmail.com>
2019-10-28 14:22:22 +0100

    still dead but alive in our hearts

--- brainfuck.s
+++ brainfuck.s
@@ -1,13 +1,16 @@
 # caschutte & ebdemir
 
-.bss
-myarray: .skip 50000000
 
 .global brainfuck
 
 .text
 format_str: .asciz "We should be executing the following code:\n%s\n"
 bfoutput: .asciz "%c"
+
+.data
+myarray: 
+.skip 30000
+
 
 # Your brainfuck subroutine will receive one argument:
 # a zero termianted string containing the code to execute.
@@ -25,8 +28,91 @@ brainfuck:
 	call printf
 	xorq %rax, %rax
 
-	#index of our array
-	xorq %rbx, %rbx
+	#index of our array set to 0 in a fancy way
+	movq $0, %r15
+
+	jmp next_action
+
+
+# print each brainfuck character
+next_action:
+
+	#store the bit value of the character into r11b
+	movb (%r12), %r11b
+
+	incq %r12
+
+	#check if we reached the end of the file
+	cmpb $0, %r11b	
+	je end
+
+	#we compare here
+	# + sign
+	cmpb $43, %r11b
+	je foundplus
+
+	# - sign
+	cmpb $45, %r11b
+	je foundminus
+
+	# > sign
+	cmpb $62, %r11b
+	je foundgreater
+
+	# < sign
+	cmpb $60, %r11b
+	je foundsmaller
+
+	# . sign 
+	cmpb $46, %r11b
+	je founddot
+
+	# [ sign 
+	cmpb $91, %r11b
+	je foundleft
+
+	# ] sign
+	cmpb $93, %r11b
+	je loop_end
+
+	jmp next_action
+		
+
+# Increments the value the pointer is pointing at
+foundplus:
+	incq myarray(%r15)	
+	jmp next_action
+
+# Decrements the value the pointer is pointing at
+foundminus:
+	decq myarray(%r15)
+	jmp next_action
+
+# Increments the pointer
+foundgreater:
+	incq %r15
+	jmp next_action
+
+# Deccrements the pointer
+foundsmaller:
+	decq %r15
+	jmp next_action
+
+
+# Prints the value the pointer is pointing at
+founddot:
+	movq $0, %rax
+	movq myarray(%r15), %rsi
+	movq $bfoutput, %rdi
+	call printf
+
+	jmp next_action
+
+foundleft:
+	cmpq $0, myarray(%r15)
+	je start_counter
+
+	push %r12
 
 	jmp next_action
 
@@ -40,63 +126,15 @@ counter:
 
 	incq %r12
 
-	movb (%r12), %r15b
+	movb (%r12), %r11b
 
-	cmpb $91, %r15b
+	cmpb $91, %r11b
 	je inc14
 
-	cmpb $93, %r15b
+	cmpb $93, %r11b
 	je dec14
 
 	jmp counter
-
-
-# print each brainfuck character
-next_action:
-
-	#store the bit value of the character into r15b
-	movb (%r12), %r15b
-
-	incq %r12
-
-	#check if we reached the end of the file
-	cmpb $0, %r15b	
-	je end
-
-	#we compare here
-	# + sign
-	cmpb $43, %r15b
-	je foundplus
-
-	# - sign
-	cmpb $45, %r15b
-	je foundminus
-
-	# > sign
-	cmpb $62, %r15b
-	je foundgreater
-
-	# < sign
-	cmpb $60, %r15b
-	je foundsmaller
-
-	# . sign 
-	cmpb $46, %r15b
-	je founddot
-
-	# , sign
-	#cmpb $44 , %r15b
-	#je foundcomma
-
-	# [ sign 
-	cmpb $91, %r15b
-	je foundleft
-
-	# ] sign
-	cmpb $93, %r15b
-	je loop_end
-
-	jmp next_action
 
 
 inc14:
@@ -107,53 +145,9 @@ inc14:
 dec14:
 	decq %r14
 	jmp counter
-		
-
-# Increments the value the pointer is pointing at
-foundplus:
-	incq myarray(%rbx)	
-	jmp next_action
-
-# Decrements the value the pointer is pointing at
-foundminus:
-
-	decq myarray(%rbx)
-	jmp next_action
-
-# Increments the pointer
-foundgreater:
-	incq %rbx
-	jmp next_action
-
-# Deccrements the pointer
-foundsmaller:
-
-	decq %rbx
-	jmp next_action
-
-# Prints the value the pointer is pointing at
-founddot:
-	# the cool way of printing
-
-	movq $0, %rax
-	movq myarray(%rbx), %rsi
-	movq $bfoutput, %rdi
-	call printf
-
-
-	jmp next_action
-
-foundleft:
-	cmpq $0, myarray(%rbx)
-	je start_counter
-
-	push %r12
-
-	jmp next_action
-
 
 loop_end:
-	cmpb $0, myarray(%rbx)
+	cmpb $0, myarray(%r15)
 	je happy_stack
 
 	popq %r12
@@ -161,7 +155,7 @@ loop_end:
 	jmp next_action
 
 happy_stack:
-	popq %r13
+	popq %rbx
 	jmp next_action
 
 
